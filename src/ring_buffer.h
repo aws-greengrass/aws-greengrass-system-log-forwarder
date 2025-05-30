6a2e49498d841e2eba03c756a7319cf73f3dec82
Archit Gupta <archigup@amazon.com>
2025-05-29 17:47:40 -0700

    Add initial ring buffer implementation (#2)
    Co-authored-by: Anubhav Rawal <rawalexe@amazon.com>

---
+++ src/ring_buffer.h
@@ -0,0 +1,26 @@
+// aws-greengrass-system-log-forwarder - System log uploader for AWS Greengrass
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Implements a single-producer, single-consumer ring buffer for log messages.
+//! Exactly one thread may call log_store_add to add entries, and exactly one
+//! thread may call log_store_get/log_store_remove to remove entries.
+
+#include <ggl/buffer.h>
+#include <ggl/error.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// TODO: Prefix the function name according to the name of the component
+// Add a log entry from the producer thread
+GglError log_store_add(GglBuffer log, uint64_t timestamp);
+
+// Get the first log entry from the consumer thread.
+// Returns false if queue is empty.
+// If returns true, must call log_store_remove after done with the entry.
+bool log_store_get(GglBuffer *log, uint64_t *timestamp);
+
+// Remove the first log entry from the consumer thread.
+// log_store_get must be called first. There must be only one call to
+// log_store_remove per call to log_store_get.
+void log_store_remove(void);
