bae34371bfce66fafe86ee3f1613c445d0ba525e
Anubhav Rawal <rawalexe@amazon.com>
2025-06-19 23:10:05 +0000

    Add working Ring buffer implementation

--- src/system-log-forwarder.h
+++ src/system-log-forwarder.h
@@ -3,18 +3,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef CLOUD_LOGGER_H
-#define CLOUD_LOGGER_H
-
-#include <ggl/arena.h>
-#include <ggl/buffer.h>
-#include <ggl/error.h>
-#include <stdio.h>
+#ifndef SYSTEM_LOG_FORWARDER_H
+#define SYSTEM_LOG_FORWARDER_H
 
 #define MAX_LINE_LENGTH (2048)
 #define THING_NAME_MAX_LENGTH (128)
 #define GGL_IPC_MAX_SVCUID_LEN (16)
-
-GglError read_log(FILE *fp, GglBuffer *filling, GglArena *alloc);
 
 #endif
