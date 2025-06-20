f9eac9132b385a7a04e3afeb450eb6b8b6f5d37c
Anubhav Rawal <rawalexe@amazon.com>
2025-06-20 21:09:11 +0000

    Add signv4 as package

--- flake.nix
+++ flake.nix
@@ -69,12 +69,12 @@
           };
 
         pname = "gg-sys-log-forwarder";
-        package = { pkgs, stdenv, pkg-config, cmake, ninja, systemdLibs, defaultMeta }:
+        package = { pkgs, stdenv, pkg-config, cmake, ninja, defaultMeta }:
           stdenv.mkDerivation {
             name = "gg-sys-log-forwarder";
             src = filteredSrc;
             nativeBuildInputs = [ pkg-config cmake ninja ];
-            buildInputs = [ systemdLibs ];
+            buildInputs = [ ];
             cmakeBuildType = "MinSizeRel";
             cmakeFlags = (fetchContentFlags pkgs) ++ [ "-DENABLE_WERROR=1" ];
 
@@ -87,11 +87,11 @@
 
         checks =
           let
-            clangBuildDir = { pkgs, pkg-config, clang-tools, cmake, systemdLibs, ... }:
+            clangBuildDir = { pkgs, pkg-config, clang-tools, cmake, ... }:
               (llvmStdenv pkgs).mkDerivation {
                 name = "clang-cmake-build-dir";
                 nativeBuildInputs = [ pkg-config clang-tools ];
-                buildInputs = [ systemdLibs ];
+                buildInputs = [ ];
                 buildPhase = ''
                   ${cmake}/bin/cmake -B $out -S ${filteredSrc} \
                     -D CMAKE_BUILD_TYPE=Debug ${toString (fetchContentFlags pkgs)}\
