aba6e6b1a79917306a4dbe271b8f6d717651cbd5
Patrick Zhang <patrzhan@amazon.com>
2025-07-16 00:25:53 +0000

    Retrieve credentials from environment variables or container credentials

---
+++ src/aws_credentials_util.h
@@ -0,0 +1,19 @@
+// aws-greengrass-system-log-forwarder - System log uploader for AWS Greengrass
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef AWS_CREDENTIALS_UTIL_H
+#define AWS_CREDENTIALS_UTIL_H
+
+#include "system-log-forwarder.h"
+#include <ggl/buffer.h>
+#include <ggl/error.h>
+
+typedef struct ContainerCredentialsInfo {
+    GglBuffer uri;
+    GglBuffer token;
+} ContainerCredentialsInfo;
+
+GglError get_credentials_chain_credentials(SigV4Details *sigv4_details);
+
+#endif
