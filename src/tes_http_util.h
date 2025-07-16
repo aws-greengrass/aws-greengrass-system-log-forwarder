80638617ffadefc16d02a35a595e2bfdbecb43e6
Patrick Zhang <patrzhan@amazon.com>
2025-07-16 00:24:57 +0000

    Add URI parsing for container credentials HTTP call

---
+++ src/tes_http_util.h
@@ -0,0 +1,16 @@
+// aws-greengrass-system-log-forwarder - System log uploader for AWS Greengrass
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TES_HTTP_UTIL_H
+#define TES_HTTP_UTIL_H
+
+#include "system-log-forwarder.h"
+#include <ggl/buffer.h>
+#include <ggl/error.h>
+
+GglError tes_http_get_credentials(
+    GglBuffer uri, GglBuffer token, SigV4Details *response
+);
+
+#endif
